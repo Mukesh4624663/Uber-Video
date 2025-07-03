80fa83c4ad8ec5c5de05015fbac844f94d599891
Mukesh4624663 <mp4624663@gmail.com>
2025-07-03 17:09:43 +0530

    user authentication done

--- Backend/app.js
+++ Backend/app.js
@@ -4,12 +4,14 @@ const cors = require("cors");
 const express = require("express");
 const connectToDb = require("./db/db");
 const userRoutes = require("./routes/user.routes");
+const cookieParser = require("cookie-parser");
 
 connectToDb();
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("Hello Worldd!");
--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,8 +1,7 @@
 const { validationResult } = require("express-validator");
 const userService = require("../services/user.service");
 const userModel = require("../models/user.model");
-
-
+const blackListTokenModel = require("../models/blacklistToken.model");
 // register
 module.exports.registerUser = async (req, res, next) => {
   const errors = validationResult(req);
@@ -32,32 +31,45 @@ module.exports.registerUser = async (req, res, next) => {
   res.status(201).json({ token, user });
 };
 
-
 // login
 module.exports.loginUser = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
 
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-    }
+  const { email, password } = req.body;
 
-    const { email, password } = req.body;
+  const user = await userModel.findOne({ email }).select("+password");
 
-    const user = await userModel.findOne({ email }).select('+password');
+  if (!user) {
+    return res.status(401).json({ message: "Invalid email or password" });
+  }
 
-    if (!user) {
-        return res.status(401).json({ message: 'Invalid email or password' });
-    }
+  const isMatch = await user.comparePassword(password);
 
-    const isMatch = await user.comparePassword(password);
+  if (!isMatch) {
+    return res.status(401).json({ message: "Invalid email or password" });
+  }
 
-    if (!isMatch) {
-        return res.status(401).json({ message: 'Invalid email or password' });
-    }
+  const token = user.generateAuthToken();
 
-    const token = user.generateAuthToken();
+  res.cookie("token", token);
 
-    res.cookie('token', token);
+  res.status(200).json({ token, user });
+};
 
-    res.status(200).json({ token, user });
-}
+// get user profile
+module.exports.getUserProfile = async (req, res, next) => {
+  res.status(200).json(req.user);
+};
+
+// logout user
+module.exports.logoutUser = async (req, res, next) => {
+  res.clearCookie("token");
+  const token = req.cookies.token || req.headers.authorization.split(" ")[1];
+
+  await blackListTokenModel.create({ token });
+
+  res.status(200).json({ message: "Logged out" });
+};
